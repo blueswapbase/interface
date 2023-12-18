e50d3ca26da71465efa5f0a64f172ede00b9cbdd
blueswapbase <141097658+blueswapbase@users.noreply.github.com>
2023-12-17 18:32:22 -0800

    Recent Changes

--- src/types/tokens.ts
+++
@@ -1,3 +0,0 @@
-import { Token } from 'blueswap-sdk-core'
-
-export class UserAddedToken extends Token {}
--- src/utils/formatPriceImpact.ts
+++
@@ -1,5 +0,0 @@
-import { Percent } from 'blueswap-sdk-core'
-
-export default function formatPriceImpact(priceImpact: Percent) {
-  return `${priceImpact.multiply(-1).toFixed(2)}%`
-}
