b99cc92fcd735867e647f64229c831ceeb49f79f
blueswapbase <141097658+blueswapbase@users.noreply.github.com>
2024-03-26 13:58:28 -0700

    Complete

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -23,7 +23,7 @@ if (typeof global.TextEncoder === 'undefined') {
 
 // Sets origin to the production origin, because some tests depend on this.
 // This prevents each test file from needing to set this manually.
-global.origin = 'https://app.uniswap.org'
+global.origin = 'https://app.lobobase.com'
 
 global.matchMedia =
   global.matchMedia ||
