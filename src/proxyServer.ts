70ca045c8414e9ccee8ba7166e301ce51864e7be
blueswapbase <141097658+blueswapbase@users.noreply.github.com>
2024-03-27 12:39:19 -0700

    modified:   src/proxyServer.ts

--- src/proxyServer.ts
+++ src/proxyServer.ts
@@ -1,26 +1,33 @@
+// proxyServer.ts
 import cors from 'cors'
 import express from 'express'
 import { createProxyMiddleware } from 'http-proxy-middleware'
 
 const app = express()
 
-app.use(
-  cors({
-    origin: 'app.lobobase.com', // Set the origin to your frontend's URL
-    credentials: true,
-  })
-)
+// CORS Configuration
+const corsOptions = {
+  origin: 'https://app.lobobase.com',
+  optionsSuccessStatus: 200,
+}
 
-app.use(
-  '/graphql',
-  createProxyMiddleware({
-    target: 'https://api.uniswap.org',
-    changeOrigin: true,
-    //pathRewrite: {
-    //  '^/graphql': '/v1/graphql', // Rewrite the URL path
-    //},
-  })
-)
+app.use(cors(corsOptions))
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => console.log(`Proxy server running on port ${PORT}`))
+// Proxy configuration
+const proxyOptions = {
+  target: 'https://api.uniswap.org', // Target server
+  changeOrigin: true, // Changes the origin of the host header to the target URL
+  onProxyReq: (proxyReq: { setHeader: (arg0: string, arg1: string) => void }) => {
+    // Set 'Origin' header to 'app.uniswap.org' for all proxied requests
+    proxyReq.setHeader('Origin', 'https://app.uniswap.org')
+  },
+}
+
+// Use the proxy middleware for requests to '/graphql'
+app.use('*', createProxyMiddleware(proxyOptions))
+
+// Start the server
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Proxy server listening on port ${PORT}`)
+})
